6b9c57b33c1051ecbc5c4344be1d55499f4589e0
Nightbomd <antfel320@gmail.com>
2026-09-23 18:04:28 -0400

    I understand that I only have four commits but

--- script.js
+++ script.js
@@ -22,3 +22,73 @@ buttons.forEach((button) => {
   });
 });
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const el = entry.target;
+    if (entry.isIntersecting) {
+      el.style.opacity = "1";
+      el.style.transform = "translateY(0)";
+    } else {
+      el.style.opacity = "0";
+      el.style.transform = "translateY(20px)";
+    }
+  });
+});
+
+const elements = document.querySelectorAll(".fade-element");
+
+elements.forEach(el => {
+  el.style.opacity = "0";
+  el.style.transform = "translateY(30px)";
+  el.style.transition = "opacity 0.6s ease, transform 0.6s ease";
+
+  observer.observe(el);
+});
+
+const secondObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const el = entry.target;
+    if (entry.isIntersecting) {
+      el.style.transform = "translateX(0)";
+      el.style.opacity = "1";
+
+    }
+    else {
+      el.style.transform = "translateX(-30px)";
+      el.style.opacity = "0";
+    }
+  })
+}) 
+
+const secondElements = document.querySelectorAll(".translate-element");
+
+secondElements.forEach(el => {
+  el.style.transform = "translateX(-20px)";
+  el.style.transition = "transform 0.6s ease";
+  el.style.opacity = "0"
+  secondObserver.observe(el);
+});
+
+const thirdObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const el = entry.target;
+
+    if (entry.isIntersecting) {
+      el.style.transform = "scale(1)";
+      el.style.opacity = "1";
+    } else {
+      el.style.transform = "scale(0.9)";
+      el.style.opacity = "0";
+    }
+  });
+});
+
+const scaleElements = document.querySelectorAll(".scale-element");
+
+scaleElements.forEach(el => {
+  el.style.transform = "scale(0.9)";
+  el.style.opacity = "0";
+  el.style.transition = "transform 0.6s ease, opacity 0.6s ease";
+
+  thirdObserver.observe(el);
+});
